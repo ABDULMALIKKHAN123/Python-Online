af73971b99a91e11e352a65411a9b21115819996
abdul malik khan <am6690707@gmail.com>
2026-07-05 09:41:07 +0530

    prob 9

---
+++ Problem9.py
@@ -0,0 +1,9 @@
+import pyttsx3
+engine = pyttsx3.init()
+
+# For Mac, If you face error related to "pyobjc" when running the `init()` method :
+# Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
+
+engine.say(" Twinkle twinkle little star How I wonder what you are Up above the world so high Like a diamond in the sky " \
+"Twinkle twinkle little starHow I wonder what you are Twinkle twinkle little star Shining brightly and afar Twinkle star dust all aroundFrom the sky right to the ground Twinkle twinkle little star Shining brightly and afar")
+engine.runAndWait()
