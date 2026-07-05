aa0be473a45fa066116842d9e203f8067eb7f7b6
abdul malik khan <am6690707@gmail.com>
2026-07-05 09:17:44 +0530

    prob 8

--- Problem8.py
+++ Problem8.py
@@ -1,3 +1,9 @@
-a = 34
-b = 80
-print ( a>b)
+# a = 34
+# b = 80
+# print ( a>b)
+
+# aur systamatic tareeqe se chahiye to ye wala sahi rahega
+
+a = input("Enter Number 1:  ")
+b = input("Enter Number 2 : ")
+print ( " a is grater than b is ", int(a)>int(b))
