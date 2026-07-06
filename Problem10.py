751349db6f6fa118fb02a8dfa9f7a241153b344b
abdul malik khan <am6690707@gmail.com>
2026-07-06 07:06:07 +0530

    prob10

---
+++ Problem10.py
@@ -0,0 +1,4 @@
+a = int (input ("Enter your Number : "  ))
+print ( "square : ",(a**2))  #kisi ke power kitna hi kyon na ho isse sab ho jayega + square bhi ho jayega  last 4 digit tak ke power ka hi ho payega
+print ( "square : ", (a*a)) 
+print ( "square : ", (a^a))  #galat hai 
