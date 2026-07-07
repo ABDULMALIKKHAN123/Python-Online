a7e2629b79f282a223d22bd68354a48c51c107c8
abdul malik khan <am6690707@gmail.com>
2026-07-07 15:18:43 +0530

    negative slicing

---
+++ 10_negative slicing .py
@@ -0,0 +1,11 @@
+name = "Abdul"
+print (name[0:3])
+
+print (name[-4:-1])
+# upar aur niche wala same hai
+print (name[1:4])
+
+
+print( name[:4])    # is same as print (name[1:5])
+print ( name[1:])   # is same as print (name[1:5])
+print (name[1:5])
