366178b98a80964395c0fd89e0326d178497e29f
abdul malik khan <am6690707@gmail.com>
2026-07-08 06:41:57 +0530

    str

---
+++ 11_str_functions.py
@@ -0,0 +1,8 @@
+name = "Abdul"
+print(len(name))
+print(name.endswith("dul"))
+print(name.startswith("Ab"))
+print(name.capitalize())
+
+# a = "mohan is a good boy"
+# a.replace("good", "bad")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+name = "Abdul"
+# print(len(name))
+# print(name.endswith("dul"))
+# print(name.startswith("Ab"))
+# print(name.capitalize())
